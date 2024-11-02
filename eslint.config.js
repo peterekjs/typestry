927079dadc2fde71b63376fbd7f4505b46c89ce2
Jiri Peterek <jiri@peterek.net>
2024-11-02 16:23:42 +0100

    style: remove prettier & use stylistic eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,31 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import stylistic from '@stylistic/eslint-plugin'
+import tseslint from 'typescript-eslint'
 
 export default [
-  {files: ['**/*.{js,ts}']},
-  {ignores: ['**/dist/*']},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { files: ['**/*.{js,ts}'] },
+  { ignores: ['**/dist/*'] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  stylistic.configs.recommended,
+  {
+    rules: {
+      '@stylistic/generator-star-spacing': ['error', { before: true }],
+    },
+  },
   {
     rules: {
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-empty-object-type': 'off',
-      '@typescript-eslint/no-explicit-any': 'off'
-    }
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
   },
   {
     files: ['**/*.spec.{js,ts}', '**/test/*.{js,ts}'],
     rules: {
-      '@typescript-eslint/no-unused-expressions': 'off'
-    }
-  }
-];
+      '@typescript-eslint/no-unused-expressions': 'off',
+    },
+  },
+]
